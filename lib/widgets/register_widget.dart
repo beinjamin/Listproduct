05edbab63d21903b3da137858bb8b45cd64b9923
Jaures <jauresbeinajmin@gmail.com>
2022-07-23 00:10:24 +0100

    Mise en place style

--- lib/widgets/register_widget.dart
+++ lib/widgets/register_widget.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:listproduct/utils.dart';
 
 class RegisterWidget extends StatelessWidget {
-TextEditingController emailcontroller = TextEditingController();
-TextEditingController passwordcontroller = TextEditingController();
-TextEditingController usernamecontroller = TextEditingController();
-
-
+  TextEditingController emailcontroller = TextEditingController();
+  TextEditingController passwordcontroller = TextEditingController();
+  TextEditingController usernamecontroller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    returnColumn(
+    return Column(
       children: [
         Container(
           width: MediaQuery.of(context).size.width,
@@ -25,7 +24,22 @@ TextEditingController usernamecontroller = TextEditingController();
                 border: InputBorder.none),
           ),
         ),
-        const SizedBox(height: 20),
+        const SizedBox(height: 18),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          margin: const EdgeInsets.only(left: 40, right: 40),
+          child: TextFormField(
+            controller: usernamecontroller,
+            style: textStyle(16, Colors.black, FontWeight.w500),
+            decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.grey[250],
+                hintText: "Username",
+                hintStyle: textStyle(16, Colors.grey, FontWeight.w500),
+                border: InputBorder.none),
+          ),
+        ),
+        const SizedBox(height: 18),
         Container(
           width: MediaQuery.of(context).size.width,
           margin: const EdgeInsets.only(left: 40, right: 40),
@@ -47,10 +61,11 @@ TextEditingController usernamecontroller = TextEditingController();
           child: TextButton(
             onPressed: () {},
             style: TextButton.styleFrom(backgroundColor: Colors.lightBlue[200]),
-            child: Text("Login",
-                style: textStyle(20, Colors.white, FontWeight.bold)
-                ),
+            child: Text("Register",
+                style: textStyle(20, Colors.white, FontWeight.bold)),
           ),
         )
       ],
     );
+  }
+}
